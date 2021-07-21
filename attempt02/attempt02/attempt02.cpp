4457821bafca5be2582c294ce082674389759322
neo-mcbraida <56191826+neo-mcbraida@users.noreply.github.com>
2021-07-21 18:03:51 +0100

    added comments.

--- attempt02/attempt02/attempt02.cpp
+++ attempt02/attempt02/attempt02.cpp
@@ -1,6 +1,4 @@
 // NNattempt02.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
 #include <vector>
 
@@ -15,20 +13,19 @@ public:
         rawInputNodes = _rawInputNodes;
         for (int i = 0; i < rawInputNodes.size(); i++) {
             weights.push_back(0.5);//default to 0.5, (completely arbitrary number)
-            //rawInputNodes.push_back(_rawInputNodes[i]);
-            std::cout << "rawInputNodes addresses: " << rawInputNodes[0] << std::endl;////delete this
         }
     }
+
+    //sets the value of the node
     void SetNode() {
         float _rawVal = 0;
         for (int i = 0; i < weights.size(); i++) {
-            std::cout << "node address to be used: " << rawInputNodes[i] << std::endl;
             float prevNode = *(rawInputNodes[i]);
             float temp = weights[i] * prevNode;
             _rawVal += temp;
         }
         rawVal = _rawVal;
-        ///
+        ///(below) for debuggin purposes
         std::cout << "raw value: " << rawVal << std::endl;
         ///
     }
@@ -46,15 +43,19 @@ public:
         width = _width;
         inpLayers = _inpLayers;
     }
+
+    //adds layer ptr to chain of pointers
     virtual void AddLayer(Layer* layer) {
         if (nextLayer == NULL) {
             SetNextLayer(layer);
             (*layer).SetPreviousLayer(this);////////////////////////////
         }
         else {
-            NextLayer(layer);
+            NextLayer();
         }
     }
+
+    //instantiates and adds n number of nodes
     virtual void AddNodes() {
         vector<float*> nodeptrs = GetNodePtrs();
         for (int i = 0; i < width; i++) {
@@ -63,58 +64,58 @@ public:
             nodes.push_back(node);
         }
     }
+
+    //sets pointer to the next layer
     void SetNextLayer(Layer* layer) {
         nextLayer = layer;
     }
-    void NextLayer(Layer* adress) {
 
-    }
+    //sets pointer to the previous layer
     void SetPreviousLayer(Layer* ptr) {
         prevLayer = ptr;
     }
+
+    //for each node in layer, set value of the node
+    //the calls method to repeat this for next layer
     virtual void SetNodes() {
         for (int i = 0; i < nodes.size(); i++) {
             nodes[i].SetNode();
         }
         NextLayer();
     }
+
+    //invokes method to set values of all nodes in the next layer, unless
+    //there is no next layer, (then i need to make it output values of node in
+    //current layer
     void NextLayer() {
-        //Layer l = *prevLayer;///////////////error reading prev ptr
-        //std::cout << l.nodes[0].rawVal << std::endl;
         if (nextLayer != NULL) {
-            Layer l = *nextLayer;
             (*nextLayer).SetNodes();
         }
     }
 private:
-    vector<float*> GetNodePtrs() {//somewhere in here there is an error
+    //Returns a 1d array of all pointers to all nodes required for the current layer
+    vector<float*> GetNodePtrs() {
         vector<float*> nodePtrs;
         int _prevlayerInd = layerIndex - 1;//layers start from one
         Layer* _prevLayerPtr = prevLayer;
         Layer _prevLayer = *prevLayer;
-        //Layer _prevLayer = *_prevLayerPtr;
         while (_prevlayerInd > 0) {
             if (std::find(inpLayers.begin(), inpLayers.end(), _prevlayerInd) != inpLayers.end()) {
-               // std::cout << "address from temp" << & (_prevLayer.nodes[0].rawVal) << std::endl;
-               std::cout << "address from pointer to layer: " << &((*_prevLayerPtr).nodes[0].rawVal) << std::endl;
-                
                 vector<float*> shortPtrs = (*_prevLayerPtr).GetLayerNodePtr();
-                //vector<float*> shortPtrs = _prevLayer.GetLayerNodePtr();
                 nodePtrs.insert(nodePtrs.end(), shortPtrs.begin(), shortPtrs.end());
             }
             _prevlayerInd--;
-            //_prevLayer = *_prevLayerPtr;
         }
         std::reverse(nodePtrs.begin(), nodePtrs.end());
         return nodePtrs;
     }
+
+    //returns 1d array of pointers to all nodes required from a specific layer
     vector<float*> GetLayerNodePtr() {
         vector<float*> nodePtrs;
         for (int i = 0; i < nodes.size(); i++) {
             float* tempPtr = &(nodes[i]).rawVal;
             nodePtrs.push_back(tempPtr);
-            std::cout << "node pointers from GetLayerNodePtr: " << nodePtrs[0] << std::endl;
-            //std::cout << &(nodePtrs[i]) << std::endl;//////delete this line
         }
         return nodePtrs;
     }
@@ -122,20 +123,16 @@ private:
 
 class HiddenLayer : public Layer {
 public:
-    //vector<int> inputLayers;
     HiddenLayer(int _width, vector<int> _inputLayers) : Layer(_width, _inputLayers) {
-        //inputLayers = _inputLayers;
     }
 private:
 };
 
 class Dense : public HiddenLayer {
 public:
-    //Layer* prevLayer;
     Dense(int _width, vector<int> _inputLayers) : HiddenLayer(_width, _inputLayers) {
 
     }
-
 private:
 };
 
@@ -144,27 +141,32 @@ public:
     Input(int _width = 0) : Layer(_width) {
         AddNodes();
     }
+
+    //adds layer pointer to the chain of pointers
+    //takes new layer pointer as argument
     void AddLayer(Layer* layer) {
         if (nextLayer == NULL) {
-            nextLayer = layer;//create addpreviousptr method
+            nextLayer = layer;
             (*layer).SetPreviousLayer(this);
         }
         else {
             (*nextLayer).AddLayer(layer);
         }
     }
+
+    //adds nodes to the layer, depending on it's width
     void AddNodes() override {
         for (int i = 0; i < width; i++) {
             Node n({});
-            //std::cout << "addNodes: " << & (n.rawVal) << std::endl;
             nodes.push_back(n);
         }
     }
+
+    //for every node in layer, calculates value of that node
+    //then calls function to repeate this for next layer
     void SetNodes(vector<float> inputData) {
         for (int i = 0; i < inputData.size(); i++) {
-            std::cout << "before setting input nodes: " << &(nodes[i].rawVal) << std::endl;
             nodes[i].rawVal = inputData[i];
-            std::cout << "after setting input nodes:  " << &(nodes[i].rawVal) << std::endl;
         }
         NextLayer();
     }
@@ -173,62 +175,39 @@ private:
 
 class Network {
 public:    
-    Input* inputLayer;//input layer stored by network was a copy rather than reference so pointers were not working.
+    Input* inputLayer;
     Network() {
     }
-    void Input(Input* layer) {
+    void Input(Input* layer) {//adds input layer to network, input layer must be first layer
         IncrementDepth();
         inputLayer = layer;
-
-        //std::cout << inputLayer << std::endl;
         (*inputLayer).layerIndex = depth;
     }
-    void HiddenLayer(Layer* layer) {
+    void HiddenLayer(Layer* layer) {//adds hidden layer to network, takes ptr as argument
         IncrementDepth();
         (*inputLayer).AddLayer(layer);
-        //std::cout << (*inputLayer).prevLayer << std::endl;
-        Layer temp = *layer;//delete this line
         (*layer).layerIndex = depth;
         (*layer).AddNodes();
     }
-    void Estimate(vector<float> inputData) {
+    void Estimate(vector<float> inputData) {//passes data to input layer, call next layers recursively
         (*inputLayer).SetNodes(inputData);
     }
 private:
-    int depth = 0;
-    void IncrementDepth() {
+    int depth = 0;//number of layers in network
+    void IncrementDepth() {//does what it says on the tin
         depth++;
     }
 };
 
 int main()
-{/////////////////PROBLEM, node pointers are pointing to the wrong address, not sure why
-    //std::cout << "Hello World!\n";
-
+{
     Network myNetwork;
-    Input inp(1);
-    //std::cout << &inp << std::endl;////////////////
+    Input inp(2);
     myNetwork.Input(&inp);
-    std::cout << "actual input address: " << & (inp.nodes[0].rawVal) << std::endl;
-    Dense layer1(1, { 1 });
+    Dense layer1(2, { 1 });
     myNetwork.HiddenLayer(&layer1);//input layers start from 1
-    //std::cout << &(layer1.nodes[0].rawVal) << std::endl;
-    //myNetwork.HiddenLayer();
-    Dense layer2(1, { 2 });
+    Dense layer2(2, { 1, 2 });
     myNetwork.HiddenLayer(&layer2);
-    //std::cout << &(layer2.nodes[0].rawVal) << std::endl;
-    vector<float> inputData = { 1 };
+    vector<float> inputData = { 1, 1 };
     myNetwork.Estimate(inputData);
-    std::cout << 0 << std::endl;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
